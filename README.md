c055141e18b7dddb49f5ced4c4e895c88e6f60c9
FrankYan93 <yanjiadong@brandeis.edu>
2016-09-22 18:03:27 -0400

    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-#twitterwhack
+#twitterwhack  
+video link:  
+https://youtu.be/oAOG6gWDbls  
+        
+given 2 words-
+word1 and word2  
+search only word1 and store the number of results to @scores[0]  
+search only word2 and store the number of results to @scores[1]  
+search both word1 and word2 and store the number of results to @scores[2]  
+  
+
+index-@scores[2]  
+score-@scores[0]\*@scores[0]  
+  
+Show index and score and a comment according to index.   
+Show a random tweet that was found by searching both words.  
--- app.rb
+++ app.rb
@@ -1,11 +1,37 @@
-require "sinatra"
-
+require 'sinatra'
+require 'rubygems'
+require 'oauth'
+require 'json'
+require_relative './connectionToTwitter.rb'
 
 get '/' do
-  erb :index
+    erb :index
 end
 
 post '/result' do
-  
-  erb :result
+    s1 = params['search1']
+    s2 = params['search2']
+    @word1=s1
+    @word2=s2
+    count=100
+    @tweets=[]
+    @scores=[0,0,0]
+    @searchs=["","",""]
+    @searchs[0]="\"#{s1}\" -\"#{s2}\""
+    @searchs[1]="\"#{s2}\" -\"#{s1}\""
+    @searchs[2]="\"#{s1}\" \"#{s2}\""
+    search_tweets(@searchs[0],count,0)
+    search_tweets(@searchs[1],count,1)
+    search_tweets(@searchs[2],count,2)
+    @comment=""
+    if @scores[2]==0 then
+      @comment="You got whacked!"
+    elsif @scores[2]<6 then
+      @comment="Excellent!"
+    elsif @scores[2]<90 then
+      @comment="Pretty Good!"
+    else
+      @comment="Try again!"
+    end
+    erb :result
 end
